a9dae9b7975ebd90b7f2648e4d4ffbee3068d978
Shaaf <shaaf.m.shahzad@gmail.com>
2023-11-27 12:35:33 -0500

    Updated main class to start game

--- donkey/src/main/java/com/shaafshahzad/App.java
+++ donkey/src/main/java/com/shaafshahzad/App.java
@@ -1,13 +1,8 @@
 package com.shaafshahzad;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        Game game = new Game();
+        game.startGame();
     }
 }
